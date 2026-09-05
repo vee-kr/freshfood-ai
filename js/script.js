1358848c4c8823137bfc16d2aadad927f02f109d
veegit config --global user.email korchagina.vm2009@gmail.comgit config --global user.name vee <korchagina.vm2009@gmail.com>
2026-09-05 17:01:37 +0300

    Add scan result and food status

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+  const scanResult = {
+            name: "Milk",
+            expirationDate: "2026-09-07"
+        };
+
                                     /* Scan page */
 
 const photoInput = document.getElementById("food-photo");
@@ -19,39 +24,103 @@ if (photoInput){
 if (scanButton) {
 
     scanButton.addEventListener("click", () => {
+        localStorage.setItem("scanResult", JSON.stringify(scanResult));
         window.location.href = "result.html";
     });
 }
+
+function getStatus(expirationDate) {
+    const today = new Date();
+    const expiration = new Date(expirationDate);
+
+     const difference = expiration - today;
+     const leftDays = Math.floor(difference / (3600 * 1000 * 24));
+
+
+     if (today > expiration) {
+         return "Expired";
+     }
+     else if (leftDays <= 3) {
+         return "Expiring soon!";
+     }
+     else {
+         return "Fresh";
+     }
+
+  }
 
 
                                     /* Result page */
 
 const addFoodButton = document.getElementById("add-food");
 
+const resultProduct = document.getElementById("result-product");
+const resultDate = document.getElementById("result-date");
+const resultStatus = document.getElementById("result-status");
+
+const savedScan = localStorage.getItem("scanResult");
+const food = JSON.parse(savedScan);
+
+
+if (resultProduct) {
+    resultProduct.textContent = food.name;
+    resultDate.textContent = food.expirationDate;
+    resultStatus.textContent = getStatus(food.expirationDate);
+
+}
+
+
 if (addFoodButton) {
     addFoodButton.addEventListener("click", () => {
-        const food = {
-            name: "Cheese",
-            expirationDate: "2 September 2026"
-        };
-        localStorage.setItem("food", JSON.stringify(food));
+
+        const savedFood = localStorage.getItem("food");
+
+        let foodList = [];
+
+        if (savedFood) {
+            foodList = JSON.parse(savedFood);
+            console.log(foodList);
+        }
+
+
+        foodList.push(food);
+
+        localStorage.setItem("food", JSON.stringify(foodList));
     });
 }
 
 
+
+
+
                                     /* My Food List page */
 
-const foodList = document.getElementById("food-list-container");
+const foodListContainer = document.getElementById("food-list-container");
 
-if (foodList) {
+if (foodListContainer) {
     const savedFood = localStorage.getItem("food");
     if (savedFood) {
-        const food = JSON.parse(savedFood);
+        const foodList = JSON.parse(savedFood);
 
-        foodList.innerHTML = `
+
+        foodList.forEach((food) => {
+
+            const expirationDate = new Date(food.expirationDate);
+
+            const formatDate = expirationDate.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+            });
+
+
+            const status = getStatus(food.expirationDate);
+
+            foodListContainer.innerHTML += `
         <h2>${food.name}</h2>
-        <p>Expiration Date: ${food.expirationDate}</p>
+        <p>Expiration Date: ${formatDate}</p>
+        <p>Status: ${status}</p>
         `;
+        });
     }
-
 }
